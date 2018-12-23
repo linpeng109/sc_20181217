9d2bdb53bcf1fb657d58e7211a6b912c2619f7c2
linpeng109 <linpeng109@aliyun.com>
2018-12-23 23:19:30 +0800

    20181223

--- schedulerServer/src/main/java/com/cn/feign/UserBaseProxy.java
+++ schedulerServer/src/main/java/com/cn/feign/UserBaseProxy.java
@@ -1,11 +1,11 @@
 package com.cn.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
-
+@Configuration
 @FeignClient(name = "userBaseProxy", url = "http://localhost:8081")
 public interface UserBaseProxy {
     @RequestMapping("/userbaseinfo/findall/{page}/{size}")
