63b37a0efdec065303f9adfc83d2fe1e810e1280
linpeng109 <linpeng109@aliyun.com>
2018-12-26 20:13:51 +0800

    20171226

--- configServer/src/main/java/com/cn/ConfigServerApplication.java
+++ configServer/src/main/java/com/cn/ConfigServerApplication.java
@@ -1,7 +1,7 @@
 package com.cn;
 
-import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -9,10 +9,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableConfigServer
-@Log4j2
 public class ConfigServerApplication {
     public static void main(String[] args) {
-        log.debug("Config Server is starting......");
         SpringApplication.run(ConfigServerApplication.class, args);
     }
 }
--- userbaseServer/src/main/java/com/cn/UserbaseServerApplication.java
+++ userbaseServer/src/main/java/com/cn/UserbaseServerApplication.java
@@ -1,6 +1,5 @@
 package com.cn;
 
-import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -9,10 +8,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableSwagger2
-@Log4j2
 public class UserbaseServerApplication {
     public static void main(String[] args) {
-        log.debug("User Server is starting....");
         SpringApplication.run(UserbaseServerApplication.class, args);
     }
 }
