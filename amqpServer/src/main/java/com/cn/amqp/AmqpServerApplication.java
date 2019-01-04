08c88f96a6bc92079de376990e4dac66c8e31c7e
linpeng109 <linpeng109@aliyun.com>
2019-01-04 21:53:15 +0800

    20190104

---
+++ amqpServer/src/main/java/com/cn/amqp/AmqpServerApplication.java
@@ -0,0 +1,13 @@
+package com.cn.amqp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class AmqpServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AmqpServerApplication.class, args);
+    }
+}
