e9ed865cd32cacabbdc61306d166df6940d5ef03
linpeng109 <linpeng109@aliyun.com>
2019-01-15 18:54:14 +0800

    20190115

--- securityServer/src/main/java/com/cn/security/JwtAcctokenConverter.java
+++ securityServer/src/main/java/com/cn/security/JwtAcctokenConverter.java
@@ -5,12 +5,14 @@ import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.token.AccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.stereotype.Service;
 
 import java.util.Map;
 
+@Service
 public class JwtAcctokenConverter implements JwtAccessTokenConverterConfigurer {
     @Override
     public void configure(JwtAccessTokenConverter jwtAccessTokenConverter) {
-        jwtAccessTokenConverter.s
+        jwtAccessTokenConverter.setSigningKey("12345");
     }
 }
--- securityServer/src/main/java/com/cn/web/HelloInfo.java
+++ securityServer/src/main/java/com/cn/web/HelloInfo.java
@@ -1,14 +1,22 @@
 package com.cn.web;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Log4j2
 public class HelloInfo {
-    @RequestMapping("/")
+    @Value("${spring.application.name}")
+    String SERVER_NAME;
+
+    @Value("${server.port}")
+    String PORT;
+
+    @RequestMapping("/hello")
     public String hello() {
-        return "Hello world ! Security Server";
+        String result = "The " + SERVER_NAME + " is running at " + PORT;
+        return result;
     }
 }
