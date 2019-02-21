03dbe62ef6f11075e480c5f7d64435599a439c62
linpeng109 <linpeng109@aliyun.com>
2019-02-21 10:36:26 +0800

    20190221

--- securityServer/src/main/java/com/cn/security/UserDetailsServer.java
+++ securityServer/src/main/java/com/cn/security/UserDetailsServer.java
@@ -1,7 +1,6 @@
 package com.cn.security;
 
 import com.cn.jpa.dao.UserDetailsDao;
-import com.cn.jpa.entity.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
