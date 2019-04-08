ecbd2f7f839cc0ef55ddaea06acfa4b5e7ab2b93
linpeng109 <linpeng109@aliyun.com>
2019-04-08 15:42:43 +0800

    20190408

---
+++ elasticServer/src/main/java/com/cn/elastic/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.cn.elastic;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HelloWorld {
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello ElasticSearch World!";
+    }
+}
