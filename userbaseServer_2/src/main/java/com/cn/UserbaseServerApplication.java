a12df90c5f92698a3bc710072edf951c7f014a2d
linpeng109 <linpeng109@aliyun.com>
2019-05-03 10:49:33 +0800

    20190503

--- userbaseServer_2/src/main/java/com/cn/UserbaseServerApplication.java
+++ userbaseServer_2/src/main/java/com/cn/UserbaseServerApplication.java
@@ -3,6 +3,7 @@ package com.cn;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
